f1deb475ac8bf24426ea39ffc48648d06ef0406e
elie <eliea@code-college.net>
2016-07-15 23:49:13 +0300

    Add project files.

---
+++ AttribDemo/CodeReviewAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AttribDemo
+{
+    internal class CodeReviewAttribute : Attribute
+    {
+        internal string _reviewerName { get; private set; }
+        internal string _reviewerDate
+        {
+            get { return DateTime.Today.ToString("M"); }
+            private set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+            }
+        }
+        internal bool _approved { get; private set; }
+
+        public CodeReviewAttribute(string name, string date, bool isApproved)
+        {
+            _reviewerName = name;
+            _reviewerDate = date;
+            _approved = isApproved;
+        }
+    }
+}
---
+++ AttribDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace AttribDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(AnalayzeAssembly(Assembly.GetExecutingAssembly()));
+        }
+
+        [CodeReview("Elie A", "", true)]
+        public class A { }
+
+        [CodeReview("Elie B", "", true)]
+        public class B { }
+
+        [CodeReview("Elie C", "", false)]
+        public class C { }
+
+        private static bool AnalayzeAssembly(Assembly myAssembly)
+        {
+            foreach (var attribute in myAssembly.GetTypes())
+            {
+                var attributeList = attribute.GetCustomAttributes(typeof(CodeReviewAttribute), false);
+
+                foreach (CodeReviewAttribute reviewAttribute in attributeList)
+                {
+                    Console.WriteLine($"Code review for type {attribute.Name}");
+                    Console.WriteLine($"Name: {reviewAttribute._reviewerName}, " +
+                                      $"Date: {reviewAttribute._reviewerDate}, " +
+                                      $"Approved: {reviewAttribute._approved}");
+
+                    if (reviewAttribute._approved) continue;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
